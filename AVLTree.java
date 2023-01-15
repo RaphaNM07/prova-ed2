c6edc91eac2c9f8fd4904d780f18eca150534114
breno <breno.bernstein@hotmail.com>
2023-01-15 18:03:08 -0300

    update avl tree

--- AVLTree.java
+++ AVLTree.java
@@ -12,6 +12,60 @@ public class AVLTree {
         }
       }
 
+      private Node rotateRight(Node node) {
+        Node leftChild = node.left;
+      
+        node.left = leftChild.right;
+        leftChild.right = node;
+      
+        updateHeight(node);
+        updateHeight(leftChild);
+      
+        return leftChild;
+      }
+
+      private Node rotateLeft(Node node) {
+        Node rightChild = node.right;
+      
+        node.right = rightChild.left;
+        rightChild.left = node;
+      
+        updateHeight(node);
+        updateHeight(rightChild);
+      
+        return rightChild;
+      }
+
+      private Node rebalance(Node node) {
+        int balanceFactor = balanceFactor(node);
+      
+        // Left-heavy?
+        if (balanceFactor < -1) {
+          if (balanceFactor(node.left) <= 0) {    // Case 1
+            // Rotate right
+            node = rotateRight(node);
+          } else {                                // Case 2
+            // Rotate left-right
+            node.left = rotateLeft(node.left);
+            node = rotateRight(node);
+          }
+        }
+      
+        // Right-heavy?
+        if (balanceFactor > 1) {
+          if (balanceFactor(node.right) >= 0) {    // Case 3
+            // Rotate left
+            node = rotateLeft(node);
+          } else {                                 // Case 4
+            // Rotate right-left
+            node.right = rotateRight(node.right);
+            node = rotateLeft(node);
+          }
+        }
+      
+        return node;
+      }
+
       
 
 }
